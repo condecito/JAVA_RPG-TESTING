44e93ff538a3f47b66a193f20ae7d0abd2feec6c
Edwin Argueta <ronyargueta@yahoo.es>
2019-12-11 09:59:43 -0600

    Update RPGTest.java

--- RPGTest/src/rpgtest/RPGTest.java
+++ RPGTest/src/rpgtest/RPGTest.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package rpgtest;
 
 /**
  *
  * @author erargueta
+ Edwin Argueta from HN
  */
 public class RPGTest {
 
@@ -15,26 +12,24 @@ public class RPGTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // dependencias : programa :CSRV037 tipo:PGM  libreria:SRVSRC    atributo CLP 
-        String server = "150.150.6.7";//"150.150.60.233"; //150.150.6.7 
-        String user = "ATL1ERAA";//ATLUCAE";
-        String pass = "c0mpu123";//a25216cae";
-        String libreria = "FRAN3"; //"AOGSRC";// "QMQMSAMP";
-        String programa = "j47145DTC";//"PBORUT";// "MATH0200C";
-        String tipoPrograma = "PGM";
-        String[] parametros = {"S|47|L|C|08|0001|*", "","",""};//{"002211375", "4551309999990008", "", ""};//{"QM.ATH.PRODUCCION", "LQ.SOL.WEB", "LQ.RES.BAT", "0100*************498400080105********ABK00222**0****************************************************************", ""};
+    //server configurations
+        String server = "";
+        String user = "";
+        String pass = "";
+   //Main RPG to call
+        String libreria = ""; 
+        String programa = "";
+        String tipoPrograma = "";
+   //params to send RPG function
+        String[] parametros = {"IN", "OUT","OUT","OUT"};
         callRPG rpg = new callRPG(server, user, pass);
-        String[] dependencia = {"SRVSRC/CSRV037/PGM"};
-        String[] desencriptado = {"FRAN3/j47145DTCX/PGM"};
-
-        // rpg.cargarLibreria("SRVSRC", "CSRV037", "PGM");
+   //if RPG program need load libraries 
+        // rpg.cargarLibreria("LIBRARI", "PROGRAM", "TYPE");
+   //call MAIN  RPG program
         rpg.testRPG("SRVSRC", "PSRSPWB", tipoPrograma, parametros);
-        rpg.imprimirSalida(1,2,3);
-        // rpg.CommandCall(dependencia);
-        // rpg.testRPG(libreria, programa, tipoPrograma, parametros);
-        //rpg.cargarLibreria("SRVSRC", "CSRV037", "PGM");
-        //  rpg.testRPG("AOGSRC", "PRUEBA2", "PGM", parametros);
-        //rpg.CommandCall(desencriptado);
+  //print out params                                         0     1     2     3
+        rpg.imprimirSalida(1,2,3);//  String[] parametros = {"IN", "OUT","OUT","OUT"};
+       
 
     }
 
